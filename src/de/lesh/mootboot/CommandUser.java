7edd6bb38dc7c00ad4de80c0769d0fe15a21aa28
Lesh <gamelesh@gmail.com>
2017-05-25 19:40:43 +0200

    Update

--- src/de/lesh/mootboot/CommandUser.java
+++ src/de/lesh/mootboot/CommandUser.java
@@ -5,6 +5,7 @@ import de.lesh.mootboot.commands.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class CommandUser implements Command{
