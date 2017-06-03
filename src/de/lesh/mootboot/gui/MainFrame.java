eb73f1b3fd1137df328533cd514c3eb531f5e895
MDWay <roman.graef@gmail.com>
2017-06-03 18:13:33 +0200

    Create MainFrame
    - Does currently nothing

---
+++ src/de/lesh/mootboot/gui/MainFrame.java
@@ -0,0 +1,12 @@
+package de.lesh.mootboot.gui;
+
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame {
+	public MainFrame (JDA jda) {
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+}
