ecc95a0d00e23d628bce9c46068f3160036a8203
Lesh <gamelesh@gmail.com>
2017-06-14 00:38:11 +0200

    o Clearing some code

--- src/de/lesh/mootboot/commands/LoveTest.java
+++ src/de/lesh/mootboot/commands/LoveTest.java
@@ -1,22 +1,19 @@
 package de.lesh.mootboot.commands;
 
+import java.awt.Color;
 import java.util.Arrays;
 import java.util.Random;
-
 import de.lesh.mootboot.user.bannedList;
 import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class LoveTest extends ListenerAdapter{
-	
 	public void onMessageReceived(MessageReceivedEvent e){
 		Message msg = e.getMessage();
 		User u = e.getAuthor();
-		
 		if(!msg.getRawContent().startsWith("-love") || bannedList.black.contains(e.getAuthor().getIdLong()) || e.getAuthor().isBot()) {
 			return;
 		}
@@ -26,17 +23,18 @@ public class LoveTest extends ListenerAdapter{
 		Random rdm = new Random();
 		int prozent = rdm.nextInt(101);
 		
-		if(words.length == 1){
+		if(words.length == 2){
 			eB.setAuthor("Dein Liebestest", null, u.getEffectiveAvatarUrl());
 			eB.addField("User: " + Arrays.toString(words), prozent + "%", true);
 			e.getChannel().sendMessage(eB.build()).queue();
-		} else if(words.length == 2){
+		} else if(words.length == 3){
 			eB.setAuthor("Der Liebestest", null, u.getEffectiveAvatarUrl());
 			eB.addField(Arrays.toString(words), prozent + "%", true);
 			e.getChannel().sendMessage(eB.build()).queue();
 		} else{
 			eB.setAuthor("ERROR >> Missing arguments", null, null);
 			eB.addField("Es fehlen Argumente für -love", "" , false);
+			eB.setColor(Color.RED);
 			e.getChannel().sendMessage(eB.build()).queue();
 		}
 	}
