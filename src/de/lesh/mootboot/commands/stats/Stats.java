f0fc38f838f4a910a68769989cb10ba11db66d8a
Lesh <gamelesh@gmail.com>
2017-06-14 21:07:22 +0200

    Adding Infos

--- src/de/lesh/mootboot/commands/stats/Stats.java
+++ src/de/lesh/mootboot/commands/stats/Stats.java
@@ -4,10 +4,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 
-import com.fasterxml.jackson.databind.cfg.MapperConfig;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import de.lesh.mootboot.user.bannedList;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.User;
@@ -15,13 +13,12 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class Stats extends ListenerAdapter{
+	
+	public String info = "https://stackoverflow.com/questions/5490789/json-parsing-using-gson-for-java";
 
 	public String path = "stats.json";
 	GsonBuilder builder = new GsonBuilder();
 	Gson gson = new Gson();
-	MapperConfig mapper = new MapperConfig<MapperConfig<T>>() {
-	};();
-	// THIS IS A TEST PLACE -- DONT COMMAND THIS
 	
 	public void onMessageReceived(MessageReceivedEvent e){
 		Template uInfo = new Template();
@@ -32,14 +29,15 @@ public class Stats extends ListenerAdapter{
 			return;
 		}
 		
+		//JsonElement jelement = new JsonParser().parse(jsonLine);
+		
+		//if()
 		uInfo.name = u.getName();
 		
 		try (Writer writer = new FileWriter(path)){
-			mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
 			builder.create();
 			gson.toJson(uInfo, writer);
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		System.out.println(gson.toJson(uInfo));
